fd3194f0ce7f7bacc3a0b0ded4d1a0591c11db8b
mingcmu <mingl3@andrew.cmu.edu>
2017-10-12 18:36:53 -0400

    super admin UI demo

---
+++ Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Inspinia_MVC5_SeedProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewData["SubTitle"] = "Main page for super admin";
+            ViewData["Message"] = "Click the different page for next step";
+
+            return View();
+        }
+
+        public ActionResult Registration()
+        {
+            ViewData["SubTitle"] = "Register candidates' infro";
+            ViewData["Message"] = "Do it in batch or manually";
+
+            return View();
+        }
+
+        public ActionResult TalentList()
+        {
+            ViewData["SubTitle"] = "List of all candiates";
+            ViewData["Message"] = "Dashboard for candiate list";
+
+            return View();
+        }
+
+        public ActionResult FileUpload()
+        {
+            ViewData["SubTitle"] = "XXX";
+            ViewData["Message"] = "OOO";
+
+            return View();
+        }
+    }
+}
