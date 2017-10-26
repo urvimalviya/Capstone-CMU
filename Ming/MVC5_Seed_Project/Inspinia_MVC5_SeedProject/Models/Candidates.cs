3b76304c70241657b6052ed458895f954fda5f70
mingcmu <mingl3@andrew.cmu.edu>
2017-10-26 14:44:49 -0400

    Candidate model

---
+++ Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Models/Candidates.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Inspinia_MVC5_SeedProject.Models
+{
+    public class Candidates
+    {   
+        [Key]
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Password { get; set; }
+        public string ProjectId { get; set; }
+    }
+}
---
+++ Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Models/OurDBContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; //data entity added
+
+namespace Inspinia_MVC5_SeedProject.Models
+{
+    public class OurDBContext: DbContext //DbContext
+    {
+        public DbSet<Candidates> candidateAccount { get; set; }
+    }
+}
