5a599f6d3d49c316dbc6419af069b3db16873afd
Terry Cao <terrycao@Terrys-MacBook-Pro.local>
2017-11-02 20:49:43 -0400

    Add .gitignore, feel free to modify

---
+++ MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace Inspinia_MVC5_SeedProject.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    
+    // movies/random
+    
+}
