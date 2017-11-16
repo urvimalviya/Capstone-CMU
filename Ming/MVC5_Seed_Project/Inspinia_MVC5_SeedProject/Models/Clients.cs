c362cf307a4be7d1bf67276f3e7baf7ceba2175a
mingcmu <mingl3@andrew.cmu.edu>
2017-11-15 22:09:46 -0500

    client module

---
+++ Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Models/Clients.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Inspinia_MVC5_SeedProject.Models
+{
+    public class Clients
+    {
+        [Key]
+        public string ClientCode { get; set; }
+        public string ProviderKey { get; set; }
+        public string CustomerNumber { get; set; } //Requestor
+        public string CallBackUri { get; set; }
+    }
+}
