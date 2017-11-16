9fc6547a9503cfa205a3dbd5313da564ebcd21e7
urvimalviya <urvimalviya@gmail.com>
2017-11-16 03:09:34 -0500

    spacing

--- Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Controllers/CandidateController.cs
+++ Ming/MVC5_Seed_Project/Inspinia_MVC5_SeedProject/Controllers/CandidateController.cs
@@ -50,9 +50,7 @@ namespace Inspinia_MVC5_SeedProject.Controllers
                     Client client = db.clientAccount.Find(clientCode);
 
                     RequestController rc = new RequestController();
-                    rc.AssessmentOrderRequest(clientCode, client.ProviderKey , client.CustomerNumber,
-                        "test01", candidate.UserID, client.CallBackUri,
-                        candidate.LastName, candidate.FirstName, candidate.Email);
+                    rc.AssessmentOrderRequest(clientCode, client.ProviderKey , client.CustomerNumber, "test01", candidate.UserID, client.CallBackUri, candidate.LastName, candidate.FirstName, candidate.Email);
                 }
             }
             return View();
